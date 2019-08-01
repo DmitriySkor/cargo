44c1c51ba947532857dc1ef1d4b624c8d828159c
ValentinGor <v.gorbatenko@mail.ru>
2019-08-01 17:21:18 +0300

    Страница owner.html Добавлено переключалку На карте / В таблице. Верстка калькулятора грузоперевозчика

---
+++ assets/js/fix.js
@@ -0,0 +1,25 @@
+// Переключалка на карте в таблице
+$("#option1").click(function (e) {
+    e.preventDefault();
+    $("#result_map").addClass('active');
+    $("#result_table").removeClass('active');
+});
+
+$("#option2").click(function (e) {
+    e.preventDefault();
+    $("#result_map").removeClass('active');
+    $("#result_table").addClass('active');
+});
+
+// Калькулятор доставки появлениее результата
+$("#delivery-calculator-1").click(function (e) {
+    e.preventDefault();
+    $(".calculator-before-result-text").removeClass('active');
+    $(".calculator-result").addClass('active');
+});
+
+$("#delivery-calculator-2").click(function (e) {
+    e.preventDefault();
+    $(".calculator-before-result-text").removeClass('active');
+    $(".calculator-result").addClass('active');
+});
